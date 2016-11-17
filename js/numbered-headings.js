056b455a755e4484d5cbcae7680b2a94722c816e
tmt514 <math.tmt514@gmail.com>
2016-11-17 13:46:35 -0500

    numbered-headings-ha

--- js/numbered-headings.js
+++ js/numbered-headings.js
@@ -16,18 +16,20 @@ var getNumbering = function(title) {
 
   p = $("section.normal").children("h1,h2,h3,h4");
   var o = [];
-  $(p).each(function(e) {
+  $(p).each(function(idx, e) {
     if ($(e).is('h1')) o = [];
-    else if ($(e).is('h2')) o.slice(0, 2);
-    else if ($(e).is('h3')) o.slice(0, 3);
-    else if ($(e).is('h4')) o.slice(0, 4);
+    else if ($(e).is('h2')) o = o.slice(0, 2);
+    else if ($(e).is('h3')) o = o.slice(0, 3);
+    else if ($(e).is('h4')) o = o.slice(0, 4);
     
     if ($(e).is('h1') && o.length < 1) o = [getNumbering($(e).text())];
     while ($(e).is('h2') && o.length < 2) o.push(0);
     while ($(e).is('h3') && o.length < 3) o.push(0);
     while ($(e).is('h4') && o.length < 4) o.push(0);
 
+
     if ($(e).is('h2') || $(e).is('h3') || $(e).is('h4')) o.push(o.pop() + 1);
+    console.log(o);
     console.log(o.join('.'));
     var w = o.join('.');
     if (w[0] === '.') w = w.slice(1);

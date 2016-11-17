6ac4238f1d15bcddd09fec48fc2ef67b0f58fd3c
tmt514 <math.tmt514@gmail.com>
2016-11-17 13:04:59 -0500

    headings2

--- js/numbered-headings.js
+++ js/numbered-headings.js
@@ -1,6 +1,12 @@
 console.log('hi', $)
 
-function setNumbering(o, levels, root) {
+// copied from http://stackoverflow.com/questions/10061414/changing-width-property-of-a-before-css-selector-using-jquery
+var addRule = function(sheet, selector, styles) {
+  if (sheet.insertRule) return sheet.insertRule(selector + " {" + styles + "}", sheet.cssRules.length);
+  if (sheet.addRule) return sheet.addRule(selector, styles);
+};
+
+var setNumbering = function(o, levels, root) {
   if (!root) return;
   if (!levels) return;
   if (levels.length == 0) return;
@@ -8,6 +14,7 @@ function setNumbering(o, levels, root) {
   children = $(root).children(levels[0]);
   chopped = levels.slice(1);
   for (var i = 1; i <= children.length; i++) {
+    addRule(document.styleSheets[0], levels[0]+"#"+$(children[i-1]).attr('id')+":before", "content: '" + o + i + "'");
     console.log(o + i, children[i-1]);
     setNumbering(o + i + ".", chopped, children[i-1]);
   }
